535e3a997536dbc41213e19972e491459d0041e5
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2020-12-02 02:31:42 -0300

    Implementation of routes and parameters in the url

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import styled from 'styled-components';
+import Home from './pages/Home';
+import Sobre from './pages/Sobre';
+import Categoria from './pages/Categoria';
 
-function App() {
+
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <BrowserRouter>
+     <header>
+       <h1>Meu site de teste</h1>
+       <ul>
+         <li><Link to="/">HOME</Link></li>
+         <li><Link to="/sobre">SOBRE</Link></li>
+         <li><Link to="/categoria/esportes">ESPORTES</Link></li>
+         <li><Link to="/categoria/noticias">NOTÍCIAS</Link></li>
+         <li><Link to="/categoria/viagens">VIAGENS</Link></li>
+       </ul>
+     </header><hr/>
+
+     <Switch>
+       <Route exact path='/'>
+         <Home/>
+       </Route>
+       <Route path='/sobre'>
+         <Sobre/>
+       </Route>
+       <Route path='/categoria/:cat'>
+         <Categoria/>
+       </Route>
+     </Switch>
+
+     <hr/>
+     <footer>
+       Todos os direitos reservados.
+     </footer>
+    </BrowserRouter>
+    
   );
 }
 
